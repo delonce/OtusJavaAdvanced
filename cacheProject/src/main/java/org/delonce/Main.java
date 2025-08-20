4fc0c58f0684b6ab973cc76497082b7ce54fd29f
p_pilkevich <P_Pilkevich@step.ru>
2025-08-20 10:49:50 +0300

    feat: Добавить класс Emulator для работы с пользователем

--- cacheProject/src/main/java/org/delonce/Main.java
+++ cacheProject/src/main/java/org/delonce/Main.java
@@ -1,46 +1,13 @@
 package org.delonce;
 
-import org.delonce.cache.SoftCache;
-import org.delonce.cache.WeakCache;
+import org.delonce.cache.menu.Emulator;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
-        example1();
+        Emulator.example1();
         System.out.println();
-        example2();
-    }
-
-    public static void example1() throws IOException, InterruptedException {
-        WeakCache weakCache = new WeakCache("cacheProject/files");
-        System.out.println("Weak before GC:");
-        System.out.println(weakCache.getFromCache("notes.txt"));
-
-        System.gc();
-
-        TimeUnit.SECONDS.sleep(3);
-        System.out.println();
-        System.out.println("Weak after GC:");
-        System.out.println(weakCache.getFromCache("notes.txt"));
-
-        weakCache.loadInCache("notes.txt");
-        System.out.println();
-        System.out.println("Weak after manually load again:");
-        System.out.println(weakCache.getFromCache("notes.txt"));
-    }
-
-    public static void example2() throws IOException, InterruptedException {
-        SoftCache softCache = new SoftCache("cacheProject/files");
-        System.out.println("Soft before GC:");
-        System.out.println(softCache.getFromCache("workers.txt"));
-
-        System.gc();
-
-        TimeUnit.SECONDS.sleep(3);
-        System.out.println();
-        System.out.println("Soft after GC:");
-        System.out.println(softCache.getFromCache("workers.txt"));
+        Emulator.example2();
     }
 }
---
+++ cacheProject/src/main/java/org/delonce/cache/menu/Emulator.java
@@ -0,0 +1,40 @@
+package org.delonce.cache.menu;
+
+import org.delonce.cache.SoftCache;
+import org.delonce.cache.WeakCache;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class Emulator {
+    public static void example1() throws IOException, InterruptedException {
+        WeakCache weakCache = new WeakCache("cacheProject/files");
+        System.out.println("Weak before GC:");
+        System.out.println(weakCache.getFromCache("notes.txt"));
+
+        System.gc();
+
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println();
+        System.out.println("Weak after GC:");
+        System.out.println(weakCache.getFromCache("notes.txt"));
+
+        weakCache.loadInCache("notes.txt");
+        System.out.println();
+        System.out.println("Weak after manually load again:");
+        System.out.println(weakCache.getFromCache("notes.txt"));
+    }
+
+    public static void example2() throws IOException, InterruptedException {
+        SoftCache softCache = new SoftCache("cacheProject/files");
+        System.out.println("Soft before GC:");
+        System.out.println(softCache.getFromCache("workers.txt"));
+
+        System.gc();
+
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println();
+        System.out.println("Soft after GC:");
+        System.out.println(softCache.getFromCache("workers.txt"));
+    }
+}
